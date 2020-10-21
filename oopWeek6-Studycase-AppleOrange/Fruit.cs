e350c0ba45b68d008668f617d4f978c70786f59d
RomainGirodet <romain.girodet@edu.devinci.fr>
2020-10-21 17:07:15 +0200

    Ajoutez des fichiers projet.

---
+++ oopWeek6-Studycase-AppleOrange/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopWeek6_Studycase_AppleOrange
+{
+    class Fruit
+    {
+        double cost;
+        public double Cost
+        {
+            get { return this.cost; }
+        }
+        public Fruit()
+        {
+            cost = 10;
+        }
+    }
+}
---
+++ oopWeek6-Studycase-AppleOrange/Orange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopWeek6_Studycase_AppleOrange
+{
+
+    class Orange : Fruit
+    {
+        double cost;
+        public double Cost_Orange
+        {
+            get { return this.cost; }
+        }
+        public Orange()
+        {
+            cost = 0.25;
+        }
+    }
+}
